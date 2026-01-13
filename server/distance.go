4b56c48a03904fe3b4afc678a48c780680868d85
alph4b3th <brunoisaac2717@gmail.com>
2026-01-13 01:07:20 -0300

    :sparkles: feat: calcular distância entre pontos

--- server/distance.go
+++ server/distance.go
@@ -13,7 +13,7 @@ import "math"
 //
 //A equipe do rakstar, recomenda a utilização desta função em vez
 //de utilizar funções nativas do samp.
-func calculateDistance(x, y, z, x2, y2, z2 float32) float32 {
+func CalculateDistance(x, y, z, x2, y2, z2 float32) float32 {
 	dx := x2 - x
 	dy := y2 - y
 	dz := z2 - z
